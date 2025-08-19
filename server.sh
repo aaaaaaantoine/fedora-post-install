d7fc0f79bf232e08db8e253948c86290edb0b67b
Antoine DX <antoine.ducroux3@gmail.com>
2025-08-19 16:04:47 +0200

    Update server.sh

--- server.sh
+++ server.sh
@@ -4,7 +4,7 @@
 sudo dnf -y upgrade
 
 ## Libvirt Qemu/KVM
-sudo dnf install -y cockpit-machines cockpit-podman libvirt
+sudo dnf install -y cockpit-machines cockpit-podman libvirt pcp python3-pcp
 sudo usermod -aG libvirt $USER
 sudo systemctl enable --now libvirtd
 
